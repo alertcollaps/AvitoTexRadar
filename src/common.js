d75051dbe4df0ed2f9067cbf3ce1cc6276f82a4d
borisov.aleksey18 <borisov.aleksey18@wb.ru>
2022-11-28 13:52:49 +0300

    Create mainPage by webpack

---
+++ src/common.js
@@ -0,0 +1,8 @@
+require('./stylesheets/base.scss')
+require('./images/tech-radar-landing-page-wide.png')
+require('./images/tw-logo.png')
+require('./images/favicon.ico')
+require('./images/search-logo-2x.svg')
+require('./images/banner-image-mobile.jpg')
+require('./images/banner-image-desktop.jpg')
+import('./localfiles/radar.json')
